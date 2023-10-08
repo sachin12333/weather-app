ebc13495bb1403454e052d61d142e3ecbc1f06c2
Sachin <prajapatisachin27790@gmail.com>
2023-10-08 04:20:34 -0700

    final project

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd7db10dc73msh3ee3ddd77aa2eb2p1ee33bjsn7b307a893bb3',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+// https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API key}
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "411c8d507b3a07faf10b70bb996b245e"
